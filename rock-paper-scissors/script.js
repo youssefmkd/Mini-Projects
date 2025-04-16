199f3502ca37947bdf251a8f817d66b866fe181b
Youssef MOUKADEM <moukadem.youssef.r4i@student.ucv.ro>
2025-04-16 21:16:03 +0100

    Add comments to explain logic

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,23 +1,27 @@
+// Array of possible choices for both player and computer
 const options = ["rock", "paper", "scissors"];
 
+// Returns a random choice of the computer
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
 
+// Determines the winner of a single round
 function checkWinner(playerSelection, computerSelection){
     if (playerSelection == computerSelection) { 
-        return "Tie";
+        return "Tie"; //Same choice -> Tie
     } else if (
         (playerSelection == "rock" && computerSelection == "scissors") || 
         (playerSelection == "paper" && computerSelection == "rock") || 
         (playerSelection == "scissors" && computerSelection == "paper")){
-            return "Player";
+            return "Player";  // Player wins based on game rules
         }else{
-            return "Computer";
+            return "Computer"; // Otherwise, computer wins
         }
     } 
 
+// Plays a single round and returns a result message
 function playRound(playerSelection, computerSelection){
     const result = checkWinner(playerSelection, computerSelection);
         if(result == "Tie"){
@@ -29,22 +33,23 @@ function playRound(playerSelection, computerSelection){
         }
     }
 
+// Prompts the user to enter a valid choice (rock, paper, or scissors)
 function getPlayerChoice(){
     let validatedInput = false;
     while(validatedInput == false){
         const choice = prompt("Rock Paper Scissors");
         if (choice == null){
-            continue;
+            continue; // Id user cancels, skip and ask again
         }
         const choiceInLower = choice.toLowerCase();
         if(options.includes(choiceInLower)){
             validatedInput = true;
-            return choiceInLower;
+            return choiceInLower; // Return the valid choice
         }
     }
 }
 
-
+// Plays the full game(5 rounds) and logs the final winner
 function game(){
     let scorePlayer = 0;
     let scoreComputer = 0;
@@ -54,12 +59,16 @@ function game(){
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
         console.log("**********************");
+
+        //Update score based on round winner
         if(checkWinner(playerSelection, computerSelection) == "Player"){
             scorePlayer++;
         }else if(checkWinner(playerSelection, computerSelection) == "Computer"){
             scoreComputer++;
         }
     }
+
+    // Final result after 5 rounds
     console.log("Game over")
     if(scorePlayer > scoreComputer){
         console.log("You win the game!");
@@ -71,4 +80,5 @@ function game(){
 
 }
 
+// Start the game
 game();
